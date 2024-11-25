bc8f8bcd74add2b949390d133821f9fc4f8774c3
lroquehu <luisroque299792458@gmail.com>
2024-11-25 07:13:41 -0500

    se arreglo el error de la conexion en php, se creó el archivo main.php

--- conexion.php
+++ conexion.php
@@ -3,14 +3,11 @@
     $usuario = "root"; 
     $contraseña = "";
     $nombreBaseDeDatos = "escuela_canto";
-
-    // Crear conexión
+    
     $conn = new mysqli($host, $usuario, $contraseña, $nombreBaseDeDatos);
 
-    // Verificar la conexión
     if ($conn->connect_error) {
         die("Conexión fallida: " . $conn->connect_error);
     }
-
     $conn->set_charset("utf8");
 ?>
--- main.php
+++ main.php
@@ -3,12 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>main</title>
 </head>
 <body>
     <h1>hola mundo</h1>
-    <button>
-        click aqui
-    </button>
 </body>
 </html>
